0b3104b8c66883836a57e6fe538dd329baaeabac
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-07-11 21:08:56 +0530

    Initial Commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Daedalus\' Cookbook.';
+export const SITE_DESCRIPTION = 'A collection of recipes from Daedalus (aka Chandram Dutta).';
